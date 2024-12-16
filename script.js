9f89722df6a724faa18d904fae7484b95a71d5e7
Mohammad Asif <akonasif321@gmail.com>
2024-12-16 20:33:36 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,496 @@
+let myjokes = {
+  "error": false,
+  "amount": 10,
+  "jokes": [
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "How do you tell HTML from HTML5?\n- Try it out in Internet Explorer\n- Did it work?\n- No?\n- It's HTML5.",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 43,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "\"Knock, knock.\"\n\"Who's there?\"\n\n[very long pause]\n\n\"Java.\"",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 36,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "Have a great weekend!\nI hope your code behaves the same on Monday as it did on Friday.",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 44,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "A man is smoking a cigarette and blowing smoke rings into the air. His girlfriend becomes irritated with the smoke and says \"Can't you see the warning on the cigarette pack? Smoking is hazardous to your health!\" to which the man replies, \"I am a programmer.  We don't worry about warnings; we only worry about errors.\"",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 38,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "\"Can I tell you a TCP joke?\"\n\"Please tell me a TCP joke.\"\n\"OK, I'll tell you a TCP joke.\"",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 57,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "I've got a really good UDP joke to tell you but I don’t know if you'll get it.",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 0,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "ASCII silly question, get a silly ANSI.",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 25,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "A SQL statement walks into a bar and sees two tables.\nIt approaches, and asks \"may I join you?\"",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 5,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "Knock knock.\nWho's there?\nRecursion.\nRecursion who?\nKnock knock.",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 180,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "Four engineers get into a car. The car won't start.\nThe Mechanical engineer says \"It's a broken starter\".\nThe Electrical engineer says \"Dead battery\".\nThe Chemical engineer says \"Impurities in the gasoline\".\nThe IT engineer says \"Hey guys, I have an idea: How about we all get out of the car and get back in\".",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 132,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+            "category": "Misc",
+            "type": "twopart",
+            "setup": "Why are men like lawnmowers?",
+            "delivery": "They are very hard to get started, they make yucky smells and half the time they don't even work.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": true,
+                "explicit": false
+            },
+            "id": 189,
+            "safe": false,
+            "lang": "en"
+        },
+        {
+            "category": "Pun",
+            "type": "twopart",
+            "setup": "A grocery store cashier asked if I would like my milk in a bag.",
+            "delivery": "I told her \"No, thanks. The carton works fine\".",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 171,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Dark",
+            "type": "twopart",
+            "setup": "How do we know Jesus wasn't black?",
+            "delivery": "He would have been hung from a tree, not a cross.",
+            "flags": {
+                "nsfw": false,
+                "religious": true,
+                "political": false,
+                "racist": true,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 110,
+            "safe": false,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "twopart",
+            "setup": "Why did the Python data scientist get arrested at customs?",
+            "delivery": "She was caught trying to import pandas!",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 234,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Dark",
+            "type": "twopart",
+            "setup": "Life's like my dick.",
+            "delivery": "The more children you have, the harder it gets.",
+            "flags": {
+                "nsfw": true,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": true
+            },
+            "id": 107,
+            "safe": false,
+            "lang": "en"
+        },
+        {
+            "category": "Misc",
+            "type": "single",
+            "joke": "In Soviet Russia, gay sex gets you arrested. In America, getting arrested gets you gay sex.",
+            "flags": {
+                "nsfw": true,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": true
+            },
+            "safe": false,
+            "id": 289,
+            "lang": "en"
+        },
+        {
+            "category": "Dark",
+            "type": "twopart",
+            "setup": "What did the cannibal do after he dumped his girlfriend?",
+            "delivery": "He wiped his ass.",
+            "flags": {
+                "nsfw": true,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": true
+            },
+            "id": 158,
+            "safe": false,
+            "lang": "en"
+        },
+        {
+            "category": "Pun",
+            "type": "twopart",
+            "setup": "Mickey Mouse and Minnie Mouse are in the divorce court.\n\"Mickey\", the judge says, \"I'm sorry. I can't grant you a divorce on the grounds of insanity. Minnie seems quite sane to me.\"",
+            "delivery": "\"I didn't say she was insane\", exclaims Mickey.\n\"I said she was fucking Goofy.\"",
+            "flags": {
+                "nsfw": true,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": true
+            },
+            "id": 225,
+            "safe": false,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "Debugging: Removing the needles from the haystack.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 40,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+            "category": "Programming",
+            "type": "single",
+            "joke": "Four engineers get into a car. The car won't start.\nThe Mechanical engineer says \"It's a broken starter\".\nThe Electrical engineer says \"Dead battery\".\nThe Chemical engineer says \"Impurities in the gasoline\".\nThe IT engineer says \"Hey guys, I have an idea: How about we all get out of the car and get back in\".",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 132,
+            "safe": true,
+            "lang": "en"
+        },
+        {
+          "category": "Programming",
+          "type": "single",
+          "joke": "// This line doesn't actually do anything, but the code stops working when I delete it.",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 12,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "Being a self-taught developer is almost the same as being a cut neck chicken because you have no sense of direction in the beginning.",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 184,
+          "safe": false,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "ASCII silly question, get a silly ANSI.",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 25,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Dark",
+          "type": "single",
+          "joke": "My girlfriend's dog died, so I tried to cheer her up by getting her an identical one. It just made her more upset. She screamed at me, \"What am I supposed to do with two dead dogs?\"",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "safe": false,
+          "id": 275,
+          "lang": "en"
+      },
+      {
+          "category": "Pun",
+          "type": "single",
+          "joke": "Today, my son asked \"Can I have a book mark?\" and I burst into tears.\n11 years old and he still doesn't know my name is Brian.",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 200,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "UDP is better in the COVID era since it avoids unnecessary handshakes.",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 259,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Misc",
+          "type": "single",
+          "joke": "In Soviet Russia, gay sex gets you arrested. In America, getting arrested gets you gay sex.",
+          "flags": {
+              "nsfw": true,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": true
+          },
+          "safe": false,
+          "id": 289,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "Algorithm: A word used by programmers when they don't want to explain how their code works.",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "id": 51,
+          "safe": true,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "Your mama's so FAT she can't save files bigger than 4GB.",
+          "flags": {
+              "nsfw": false,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": true
+          },
+          "id": 9,
+          "safe": false,
+          "lang": "en"
+      },
+      {
+          "category": "Programming",
+          "type": "single",
+          "joke": "Documentation is like sex:\nWhen it's good, it's very good.\nWhen it's bad, it's better than nothing...",
+          "flags": {
+              "nsfw": true,
+              "religious": false,
+              "political": false,
+              "racist": false,
+              "sexist": false,
+              "explicit": false
+          },
+          "safe": false,
+          "id": 305,
+          "lang": "en"
+      }
+  ]
+}
+let index = Math.floor(Math.random() * (myjokes.length - 1))
+console.log(index)
+joke.innerHTML = myjokes[index].joke
